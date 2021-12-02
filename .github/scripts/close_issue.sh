74b6606c2b2cf8ccd391d93ccfed4dad813388b5
Scott Macfarlane <smacfarlane@hashicorp.com>
2021-12-02 13:05:39 -0800

    Tacos need no newlines
    Signed-off-by: Scott Macfarlane <smacfarlane@hashicorp.com>

--- .github/scripts/close_issue.sh
+++ .github/scripts/close_issue.sh
@@ -6,7 +6,7 @@ declare -r issue_url="${1}"
 
 curl \
   -X PATCH \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}"
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   "${issue_url}" \
   -d '{"state":"closed"}'
