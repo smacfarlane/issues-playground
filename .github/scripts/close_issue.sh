ea12fa92a2b8ff3cc090d87837b796b366c1b07f
Scott Macfarlane <smacfarlane@hashicorp.com>
2021-12-02 13:10:17 -0800

    Tacos like long names
    Signed-off-by: Scott Macfarlane <smacfarlane@hashicorp.com>

--- .github/scripts/close_issue.sh
+++ .github/scripts/close_issue.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 declare -r issue_url="${1}"
 
 curl \
-  -X PATCH \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "Accept: application/vnd.github.v3+json" \
+  --silent \
+  --show-error \
+  --request PATCH \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  --header "Accept: application/vnd.github.v3+json" \
   "${issue_url}" \
-  -d '{"state":"closed"}'
+  --data '{"state":"closed"}'
--- .github/scripts/post_a_taco.sh
+++ .github/scripts/post_a_taco.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 declare -r comment_url="${1}"
 
 curl \
-  -X POST \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "Accept: application/vnd.github.v3+json" \
+  --silent \
+  --show-error \
+  --request POST \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  --header "Accept: application/vnd.github.v3+json" \
   "${comment_url}" \
-  -d '{"body":"![taco](https://c.tenor.com/zBiP5wuKb8UAAAAi/taco-tuesday-taco.gif)"}'
+  --data '{"body":"![taco](https://c.tenor.com/zBiP5wuKb8UAAAAi/taco-tuesday-taco.gif)"}'
